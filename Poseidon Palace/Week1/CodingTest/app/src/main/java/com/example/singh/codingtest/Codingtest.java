3f2e07cbdca56152564b4ea91c39435abc6bc946
singh <android@manroop.com>
2017-06-20 11:12:19 -0400

    added coding test questions

---
+++ Poseidon Palace/Week1/CodingTest/app/src/main/java/com/example/singh/codingtest/Codingtest.java
@@ -0,0 +1,24 @@
+package com.example.singh.codingtest;
+
+/**
+ * Created by Android on 6/20/2017.
+ */
+
+public class Codingtest {
+    public static void main(String[] args) {
+
+
+        //Problem one
+        //Create a fundtion to print fiboncacci series
+
+
+        //Problem two
+        //Input
+        System.out.println("have a nice day");
+        //Output
+        System.out.println("day nice a have");
+
+
+    }
+
+}
---
+++ Poseidon Palace/Week1/CodingTest/app/src/main/java/com/example/singh/codingtest/abc.java
@@ -0,0 +1,14 @@
+package com.example.singh.codingtest;
+
+/**
+ * Created by singh on 6/20/17.
+ */
+
+public class abc {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
