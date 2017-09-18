23d4130b0b05ceadec83ad74357c45a811ca4acb
Manroop Singh <singh@manroop.com>
2017-09-18 13:05:17 -0400

    added retrofit to making rest calls

--- 8.28 Android/Week 3/MakingRestCalls/app/src/main/java/com/example/user/makingrestcalls/MainActivity.java
+++ 8.28 Android/Week 3/MakingRestCalls/app/src/main/java/com/example/user/makingrestcalls/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.user.makingrestcalls;
 
+import android.support.constraint.solver.SolverVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.user.makingrestcalls.model.MyResponse;
+import com.example.user.makingrestcalls.model.github.GithubRepo;
+import com.example.user.makingrestcalls.model.github.Owner;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -25,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActOkSyncTag";
     private TextView tvResult;
     private OkHttpClient client;
+    private ImageView imageView;
+
+    private String image_url = "https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvResult = (TextView) findViewById(R.id.tvResult);
+        imageView = (ImageView) findViewById(R.id.ivTest);
+
+        Glide.with(this).load(image_url).into(imageView);
 
         //create the okhttp client
         client = new OkHttpClient();
@@ -93,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
 
                         String responseAsync = response.body().string();
                         Gson gson = new Gson();
+
                         MyResponse myResponse =
                                 gson.fromJson(responseAsync, MyResponse.class);
 
@@ -108,9 +123,58 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.btnRetrofitSync:
 
+                final retrofit2.Call<List<GithubRepo>> callRepos
+                        = RetrofitHelper.createCall("manroopsingh");
+
+                //create a thread to make the rest call on a separate thread
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        try {
+                            final String repoName = callRepos.execute()
+                                    .body().get(0).getName();
+
+                            //use this method tp update the ui views
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvResult.setText(repoName);
+                                }
+                            });
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }).start();
+
+
                 break;
 
             case R.id.btnRetrofitAsync:
+
+                retrofit2.Call<Owner> getUserProfile = RetrofitHelper
+                        .createCallUser("manroopsingh");
+
+                getUserProfile.enqueue(new retrofit2.Callback<Owner>() {
+                    @Override
+                    public void onResponse(retrofit2.Call<Owner> call, final retrofit2.Response<Owner> response) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvResult.setText(response.body().getLogin());
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<Owner> call, Throwable t) {
+
+                    }
+                });
 
                 break;
         }
---
+++ 8.28 Android/Week 3/MakingRestCalls/app/src/main/java/com/example/user/makingrestcalls/RetrofitHelper.java
@@ -0,0 +1,62 @@
+package com.example.user.makingrestcalls;
+
+import com.example.user.makingrestcalls.model.github.GithubRepo;
+import com.example.user.makingrestcalls.model.github.Owner;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by singh on 9/18/17.
+ */
+
+public class RetrofitHelper {
+
+    public static final String BASE_URL = "https://api.github.com/";
+
+    //create and initialize the retrofit instance
+    public static Retrofit create() {
+        
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit;
+    }
+
+    //create a static method to use the interface verbs
+    public static Call<List<GithubRepo>> createCall(String user) {
+        Retrofit retrofit = create();
+        ApiService apiService = retrofit.create(ApiService.class);
+        return apiService.getGithubRepos(user);
+    }
+
+    public static Call<Owner> createCallUser(String user) {
+        Retrofit retrofit = create();
+        ApiService apiService = retrofit.create(ApiService.class);
+        return apiService.getGithubProfile(user);
+    }
+
+
+
+    //create an interface to have all the paths and verbs for the REST api to use
+    interface ApiService {
+
+        @GET("users/{user}/repos")
+        Call<List<GithubRepo>> getGithubRepos(@Path("user") String user);
+
+        @GET("users/{user}")
+        Call<Owner> getGithubProfile(@Path("user") String user);
+
+    }
+
+}
