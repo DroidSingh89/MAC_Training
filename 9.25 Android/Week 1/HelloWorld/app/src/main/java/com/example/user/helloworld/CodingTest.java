6557c7e3af2eb43d11c3051b12e23186cfd64744
Manroop Singh <singh@manroop.com>
2017-09-26 15:38:20 -0400

    Sdf

---
+++ 9.25 Android/Week 1/HelloWorld/app/src/main/java/com/example/user/helloworld/CodingTest.java
@@ -0,0 +1,32 @@
+package com.example.user.helloworld;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by singh on 9/26/17.
+ */
+
+public class CodingTest {
+
+
+    public static void main(String[] args) {
+
+        //Problem 1
+        // Create a function to check if the number
+        // is divisible by 3 then print "fizz"
+        // is divisible by 5 then print "buzz"
+        // is divisible by 3 and 5 then print "fizzbuzz"
+        // otherwise print the number itself
+
+        //Problem 2
+        //Create a function to find duplicate strings
+        //in a list of string and print them
+
+
+
+
+    }
+
+
+}
