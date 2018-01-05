9f8dbdd7c694ef2c1a028f9c537a90ad53f0835c
Manroop Singh <singh@manroop.com>
2018-01-05 10:52:56 -0500

    testing git push

--- 11.27 Android/Week 5/Testing/app/src/androidTest/java/com/example/user/testing/ExampleInstrumentedTest.java
+++ 11.27 Android/Week 5/Testing/app/src/androidTest/java/com/example/user/testing/ExampleInstrumentedTest.java
@@ -21,6 +21,8 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
+        Context appContext1 = InstrumentationRegistry.getTargetContext();
+
         assertEquals("com.example.user.testing", appContext.getPackageName());
     }
 }
