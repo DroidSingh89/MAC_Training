1681e77ed06bb0bf63ee3f92f705ec54d7b8470c
Manroop Singh <singh@manroop.com>
2018-02-21 18:49:54 -0500

    update week 7

---
+++ 1.8/Week 7/ConfiguringBuilds/app/src/main/java/com/example/user/configuringbuilds/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.configuringbuilds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.user.mylibrary.WickedWarriors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        TextView textView = findViewById(R.id.tvMain);
+//
+//
+//        WickedWarriors wickedWarriors = new WickedWarriors();
+//        textView.setText(wickedWarriors.goWicked());
+    }
+}
---
+++ 1.8/Week 7/ConfiguringBuilds/mylibrary/src/main/java/com/example/user/mylibrary/WickedWarriors.java
@@ -0,0 +1,12 @@
+package com.example.user.mylibrary;
+
+/**
+ * Created by singh on 2/20/18.
+ */
+
+public class WickedWarriors {
+
+    public String goWicked() {
+        return "I anm wicked";
+    }
+}
