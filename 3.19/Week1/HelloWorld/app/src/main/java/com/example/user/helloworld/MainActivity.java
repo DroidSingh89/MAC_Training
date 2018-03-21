1ef52973c81b22be9c7412bb891b9b0259342bd4
Manroop Singh <singh@manroop.com>
2018-03-21 15:11:40 -0400

    new batch hello world

---
+++ 3.19/Week1/HelloWorld/app/src/main/java/com/example/user/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        Toast.makeText(this, "Do something", Toast.LENGTH_SHORT).show();
+
+    }
+}
